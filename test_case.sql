f2d3a68a2d9caac02d847f17817560513d180acd
hspoon-aws <hspoon@amazon.com>
2023-04-18 23:06:55 +0800

    bug fix

--- test_case.sql
+++ test_case.sql
@@ -3,7 +3,6 @@ create user test_user_management identified by test_user_management;
 drop user test_user_management; 
 
 -- TC2: Create, alter, insert update
-BEGIN
 create table Customers (Name varchar2(100));
 ALTER TABLE Customers
 ADD Email varchar(255);
@@ -12,5 +11,4 @@ update Customers set Name = 'David Beckham' where  Email = 'david@example.com';
 select * from Customers;
 delete Customers;
 drop table Customers; 
-END
 
